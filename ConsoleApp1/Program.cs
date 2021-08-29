a4c56bc868cd0ec633458b314a4f52afdde52828
Alexey Romanov <51180322+silvathebest@users.noreply.github.com>
2021-08-29 20:09:18 +0400

    v 1.0

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dirPath = "D://works";
+            //Создаем файл
+            StreamWriter f = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/textFile.txt", true);
+            //Указываем нужные нам расширения
+            string[] ext = new string[2] { "*.cs", "*.xaml" };
+
+            foreach (string found in ext)
+            {
+                string[] extracted = Directory.GetFiles(dirPath, found, SearchOption.AllDirectories);
+
+                foreach (string file in extracted)
+                {
+                    //Записываем в файл строки
+                    string[] lines = File.ReadAllLines(file);
+                    foreach (string s in lines)
+                    {
+                        f.WriteLine(s);
+                    };
+                }
+            }
+            f.Close();
+        }
+    }
+}
